08615d5be4eeb8dbd4e18f223c5e249ba84abafe
Tulio <tulio2207@yahoo.com.br>
2023-12-20 10:04:48 -0300

    class resultado do evento

--- model/ResultadoEvento.java
+++ model/ResultadoEvento.java
@@ -1,5 +1,14 @@
 package model;
 
 public class ResultadoEvento {
-    
+
+	private final boolean ganhou;
+
+	public ResultadoEvento(boolean ganhou) {
+		this.ganhou = ganhou;
+	}
+
+	public boolean isGanhou() {
+		return ganhou;
+	}
 }
